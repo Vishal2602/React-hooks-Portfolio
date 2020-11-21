33e0964561bb04a52112595d447e1502d52bd60d
vishal <vishals2602@gmail.com>
2020-11-21 14:57:18 +0530

    initial commit

---
+++ src/data/menuData.js
@@ -0,0 +1,10 @@
+export const menuData = [
+  { title: "Projects", icon: "/images/icons/courses.svg", link: "/projects" },
+  { title: "Resume", icon: "/images/icons/tutorials.svg", link: "/resume" },
+  { title: "", icon: "/images/icons/account.svg", link: "/account" },
+]
+
+export const tooltipData = [
+  { title: "About", icon: "/images/icons/profile.svg", link: "/about" },
+  { title: "Contact", icon: "/images/icons/account.svg", link: "/contact" },
+]
